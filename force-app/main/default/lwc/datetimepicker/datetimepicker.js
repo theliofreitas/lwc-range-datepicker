64468fb560a0f77384fe8b48b957f7b585c0aae3
Thélio Freitas <marquesthelio@hotmail.com>
2022-03-02 16:39:43 -0300

    Handle input change and autopopulate time range

--- force-app/main/default/lwc/datetimepicker/datetimepicker.js
+++ force-app/main/default/lwc/datetimepicker/datetimepicker.js
@@ -4,4 +4,32 @@ import { defaultInputDate } from './helper';
 export default class Datetimepicker extends LightningElement {	
 	@api startDatetime = defaultInputDate().startDate;
 	@api endDatetime = defaultInputDate().endDate;
+	@api rangeInMinutes = 60;
+
+	handleStartDateChange(event) {
+		const currentStartDate = new Date(event.detail.value);
+		this.startDatetime = currentStartDate.toISOString();
+
+		const newEndDate = new Date(currentStartDate);
+		newEndDate.setMinutes(currentStartDate.getMinutes() + this.rangeInMinutes);
+		this.endDatetime = newEndDate.toISOString();
+	}
+
+	handleEndDateChange(event) {
+		const currentStartDate = new Date(this.startDatetime);
+		const currentEndDate = new Date(event.detail.value);
+
+		if (currentEndDate <= currentStartDate) {
+			const newStartDate = new Date(currentEndDate);
+			newStartDate.setMinutes(currentEndDate.getMinutes() - this.rangeInMinutes);
+		
+			this.startDatetime = newStartDate.toISOString();
+		}
+		else if (currentEndDate > currentStartDate) {
+			const hours = (currentEndDate.getHours() - currentStartDate.getHours());
+			const newRange = (currentEndDate.getMinutes() - currentStartDate.getMinutes()) + (hours * 60);
+
+			this.rangeInMinutes = newRange;
+		}
+	}
 }
